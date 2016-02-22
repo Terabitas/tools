d8296f5170c264d32f839ac45ebd3322787e2044
Tomas Bartkus <to.bartkus@gmail.com>
2016-02-22 20:59:05 +0200

    Update log

--- cmd/nildev/inout/types.go
+++ cmd/nildev/inout/types.go
@@ -45,7 +45,7 @@ func {{$f.GetHandlerName}}(rw http.ResponseWriter, r *http.Request) {
     {{if $f.In.GetFieldsSlice}}
     var requestData map[string]string
     requestData = mux.Vars(r)
-    log.Infof("Request vars [%+v]", requestData)
+    log.Info(requestData)
     {{end}}
 
 	reqDTO := &{{$f.In.Name}}{}
